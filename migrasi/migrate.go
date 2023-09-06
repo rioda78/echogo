62c04c43b499cdd208c263acbc8147bccf2b44c5
unang <acedefire@gmail.com>
2023-09-06 22:24:31 +0800

    remove response

--- migrasi/migrate.go
+++ migrasi/migrate.go
@@ -10,5 +10,4 @@ func Migrate(db *gorm.DB) {
 	db.Migrator().DropColumn(&models.User{}, "Email")
 	db.AutoMigrate(&models.User{})
 	db.AutoMigrate(&models.Permission{})
-	db.AutoMigrate(&models.Response{})
 }
